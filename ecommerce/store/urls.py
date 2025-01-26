cbc0d9c6002771d265250183fc5aab197e17397d
Kasaksaxena <kasaksaxena6@gmail.com>
2025-01-26 14:52:58 +0530

    navigation building

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -3,4 +3,7 @@ from store import views
 
 urlpatterns = [
     path("",views.home,name='home'),
+    path("men/",views.men,name='men'),
+    path("women/",views.women,name='women')
+    
 ]
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+def men(request):
+    return render(request,'men.html')
+
+def women(request):
+    return render(request,'women.html')
